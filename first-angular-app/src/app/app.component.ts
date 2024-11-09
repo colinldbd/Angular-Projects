5825c3600107c6e1f2fbd283e4a6dd6811dc5323
Colin Xuesi Peng <colinxspeng@outlook.com>
2024-11-09 16:42:06 +1100

    Added user component using ng generate component

--- first-angular-app/src/app/app.component.ts
+++ first-angular-app/src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+// This is the decorater angular use to add meta data
+// for the component, which acts as the configuration
+// for this component
 @Component({
+  // Tell angular which element this component should replace
   selector: 'app-root',
+  // Standalone means this is not module based component
+  // simpler to use, set this to no to use module based
+  // component to be compatible with contentional angular
+  // component
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'first-angular-app';
